84914758826ca094726b1a50722d114d0e9540c2
Adolph Nkula <2746156@collegelacite.ca>
2025-07-23 18:30:48 -0400

    Premier commit

---
+++ main/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace main
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Un programme simple qui affiche "Hello, GitHub! Je m'appelle Adolph Nkula"
+            Console.WriteLine("Hello, GitHub! Je m'appelle Adolph Nkula");
+        }
+    }
+}
